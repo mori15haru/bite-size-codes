9ae2677192fcbc141abed3788b47aedd328775d1
Miri Kim <miri.kim@onthebeach.co.uk>
2016-12-09 15:12:48 +0000

    Fix; Add a counter-example

--- 1-majority.rb
+++ 1-majority.rb
@@ -6,40 +6,48 @@
 # Time O(n),Space O(1)
 
 def majority(list)
+  head, *tail = list
+
   flags = {
-    element: 0,
-    count: 0
+    candidate: head,
+    count: 1
   }
 
-  list.each do |e|
-    if flags[:element] == e
+  tail.each do |e|
+    if flags[:candidate] == e
       flags[:count] += 1
     else
-      if flags[:count] == 0
-        flags[:element] = e
+      if flags[:count] == 1
+        flags[:candidate] = e
+      else
+        flags[:count] -= 1
       end
     end
   end
 
-  if list.count(flags[:element]) > list.length/2
-    flags[:element]
+  if list.count(flags[:candidate]) > list.length/2
+    flags[:candidate]
   else
     nil
   end
 end
 
-RSpec.describe'#majority' do
+RSpec.describe '#majority' do
   context 'there is a majority element' do
-    let(:list) { [1,3,4,5,4,4,2,4,4,1,4] }
-    
+    let(:list1) { [1,3,4,5,4,4,2,4,4,1,4] }
+    let(:list2) { [4,4,4,5,5,5,5] }
+    let(:list3) { [3,1,3,1,3,1,3,1,3] }
+
     it 'returns the majority element' do
-      expect(majority(list)).to eq(4)
+      expect(majority(list1)).to eq(4)
+      expect(majority(list2)).to eq(5)
+      expect(majority(list3)).to eq(3)
     end
   end
 
   context 'there is no majority element' do
     let(:list) { [1,2,3,4,4,4] }
-    
+
     it 'returns nil' do
       expect(majority(list)).to be_nil
     end
